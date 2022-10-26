bea33ca2b36ec94071acf17fbd88f86042cc3b3f
jamesgaddumcalc <112296686+jamesgaddumcalc@users.noreply.github.com>
2022-10-26 15:03:53 +1300

    Revert "accept owner when creating a vault (#78)"
    This reverts commit 7357a10f01ad89831a8abb41d045e79e1aea65b2.

--- contracts/dca/src/tests/get_trigger_id_by_fin_limit_order_idx_tests.rs
+++ contracts/dca/src/tests/get_trigger_id_by_fin_limit_order_idx_tests.rs
@@ -29,7 +29,6 @@ fn should_fetch_existing_trigger_id_by_order_idx() {
             Addr::unchecked(USER),
             mock.dca_contract_address.clone(),
             &ExecuteMsg::CreateVault {
-                owner: None,
                 price_threshold: None,
                 label: "label".to_string(),
                 destinations: None,
