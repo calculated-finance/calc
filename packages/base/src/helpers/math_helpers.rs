e82e954c56a56459e7ad3addf93705bc659a097e
James Gaddum <james@calculated.fi>
2022-10-27 16:59:07 +1300

    add tests to find edge of checked_mul

--- packages/base/src/helpers/math_helpers.rs
+++ packages/base/src/helpers/math_helpers.rs
@@ -61,4 +61,47 @@ mod tests {
 
         assert_eq!(result.unwrap(), Uint128::new(16));
     }
+
+    #[test]
+    fn multiply_when_max_decimal_should_succeed() {
+        let a = Uint128::new(10);
+        let b = Decimal::MAX;
+
+        let result = checked_mul(a, b);
+
+        assert_eq!(result.unwrap(), Uint128::new(3402823669209384634633));
+    }
+
+    #[test]
+    fn multiply_when_max_int_should_fail() {
+        let a = Uint128::MAX;
+        let b = Decimal::from_str("1.5").unwrap();
+
+        let result = checked_mul(a, b);
+
+        assert_eq!(result.unwrap_err(), CheckedMultiplyRatioError::Overflow);
+    }
+
+    #[test]
+    fn multiply_when_max_decimal_and_large_int_should_fail() {
+        let a = Uint128::new(10).checked_pow(38).unwrap();
+        let b = Decimal::MAX;
+
+        let result = checked_mul(a, b);
+
+        assert_eq!(result.unwrap_err(), CheckedMultiplyRatioError::Overflow);
+    }
+
+    #[test]
+    fn multiply_when_small_decimal_and_large_int_should_succeed() {
+        let a = Uint128::new(10).checked_pow(38).unwrap();
+        let b = Decimal::from_str("0.015").unwrap();
+
+        let result = checked_mul(a, b);
+
+        assert_eq!(
+            result.unwrap(),
+            Uint128::new(1500000000000000000000000000000000000)
+        );
+    }
 }
