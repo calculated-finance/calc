bd30c1a8429cb8825471af8fc16a23cc91a4cd53
aidan <aidan@calculated.fi>
2022-11-08 19:11:21 +1100

    automation fees

--- contracts/dca/src/handlers/after_z_delegation.rs
+++ contracts/dca/src/handlers/after_z_delegation.rs
@@ -1,12 +1,14 @@
 use crate::error::ContractError;
 use crate::state::cache::CACHE;
+use crate::state::config::get_config;
 use crate::state::events::create_event;
 use crate::state::vaults::get_vault;
 use base::events::event::{EventBuilder, EventData};
+use base::helpers::math_helpers::checked_mul;
 use base::helpers::message_helpers::{
     get_attribute_in_event, get_coin_from_display_formatted_coin,
 };
-use cosmwasm_std::SubMsgResult;
+use cosmwasm_std::{SubMsgResult, Coin, Uint128, CosmosMsg, BankMsg};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::{DepsMut, Env, Reply, Response};
 
@@ -17,6 +19,9 @@ pub fn after_z_delegation(
 ) -> Result<Response, ContractError> {
     let cache = CACHE.load(deps.storage)?;
     let vault = get_vault(deps.storage, cache.vault_id.into())?;
+    let mut response = Response::new()
+        .add_attribute("method", "after_z_delegation")
+        .add_attribute("vault_id", vault.id.to_string());
 
     match reply.result {
         SubMsgResult::Ok(_) => {
@@ -28,7 +33,22 @@ pub fn after_z_delegation(
             let display_formatted_coin =
                 get_attribute_in_event(&z_delegate_response.events, "delegate", "amount")?;
 
-            let delegation_amount = get_coin_from_display_formatted_coin(display_formatted_coin);
+            let delegation = get_coin_from_display_formatted_coin(display_formatted_coin);
+
+            let config = get_config(deps.storage)?;
+
+            let automation_fee = Coin::new(
+                checked_mul(delegation.amount.clone(), config.automation_fee_percent)?.into(),
+                &delegation.denom,
+            );
+
+            // never try to send 0 tokens
+            if automation_fee.amount.gt(&Uint128::zero()) {
+                response = response.add_message(CosmosMsg::Bank(BankMsg::Send {
+                    to_address: config.fee_collector.to_string(),
+                    amount: vec![automation_fee.clone()],
+                }));
+            }
 
             create_event(
                 deps.storage,
@@ -37,7 +57,7 @@ pub fn after_z_delegation(
                     env.block,
                     EventData::DcaVaultZDelegationSucceeded {
                         validator_address,
-                        delegation: delegation_amount,
+                        delegation,
                     },
                 ),
             )?;
@@ -50,7 +70,5 @@ pub fn after_z_delegation(
         }
     }
 
-    Ok(Response::new()
-        .add_attribute("method", "after_z_delegation")
-        .add_attribute("vault_id", vault.id.to_string()))
+    Ok(response)
 }
