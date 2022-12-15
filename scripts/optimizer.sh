3aee52fbc80526556065d03af4954446c2ed1ed4
aidan <aidan@calculated.fi>
2022-12-15 16:59:53 +1000

    add manual step

--- scripts/optimizer.sh
+++ scripts/optimizer.sh
@@ -41,6 +41,7 @@ then
       LATEST_VERSION=${BASH_REMATCH[1]}
       LATEST_RC=${BASH_REMATCH[2]}
       LATEST_RC_PLUS_ONE=$(($LATEST_RC + 1))
+      echo "new rc $LATEST_RC_PLUS_ONE"
       TAG=$LATEST_VERSION-rc.$LATEST_RC_PLUS_ONE+$BUILD_HASH
       echo "latest version: $LATEST_VERSION"
       echo "latest rc: $LATEST_RC"
@@ -62,7 +63,27 @@ then
       echo "failed to match any versions"
     fi
   fi
+elif [ $# -gt 0 ] && [ $1 = "--tag-manual" ]
+  then
+    YEAR=$(date '+%y')
+    MONTH=$(date '+%m')
+    DAY=$(date '+%d')
+    echo "generating build hash..."      
+    OUTPUT=$(docker run --rm -v "$(pwd)":/code \
+      --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+      --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+      cosmwasm/workspace-optimizer:0.12.9)
 
+    regex='(.{65}) dca.wasm'
+    if [[ $OUTPUT =~ $regex ]]
+    then
+      BUILD_HASH=$(echo $BASH_REMATCH[1] | cut -c 1-7)
+      echo "build hash: $BUILD_HASH"
+
+      TAG=$2+$BUILD_HASH
+      git tag -a $TAG -m "testnet $DAY.$MONTH.$YEAR"
+      git push origin $TAG
+    fi
 else
   docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
