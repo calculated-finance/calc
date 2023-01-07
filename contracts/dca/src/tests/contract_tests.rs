ca1881b17f8bc090fa5d02f79ba4f7572fc957d5
aidan <aidan@calculated.fi>
2023-01-07 11:42:08 +1100

    instantiate tests

--- contracts/dca/src/tests/contract_tests.rs
+++ contracts/dca/src/tests/contract_tests.rs
@@ -75,6 +75,57 @@ fn instantiation_with_invalid_admin_address_should_fail() {
 }
 
 #[test]
+fn instantiation_with_invalid_fee_collector_address_should_fail() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+    let info = mock_info("", &vec![]);
+
+    let instantiate_message = InstantiateMsg {
+        admin: Addr::unchecked(VALID_ADDRESS_ONE),
+        fee_collectors: vec![FeeCollector {
+            address: Addr::unchecked(INVALID_ADDRESS),
+            allocation: Decimal::from_str("1").unwrap(),
+        }],
+        swap_fee_percent: Decimal::from_str("0.015").unwrap(),
+        delegation_fee_percent: Decimal::from_str("0.0075").unwrap(),
+        staking_router_address: Addr::unchecked(VALID_ADDRESS_ONE),
+        page_limit: 1000,
+        paused: false,
+    };
+
+    let result = instantiate(deps.as_mut(), env, info, instantiate_message).unwrap_err();
+
+    assert_eq!(
+        result.to_string(),
+        "Error: fee collector address  is invalid"
+    )
+}
+
+#[test]
+fn instantiation_with_fee_collector_amounts_not_equal_to_100_percent_should_fail() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+    let info = mock_info("", &vec![]);
+
+    let instantiate_message = InstantiateMsg {
+        admin: Addr::unchecked(VALID_ADDRESS_ONE),
+        fee_collectors: vec![],
+        swap_fee_percent: Decimal::from_str("0.015").unwrap(),
+        delegation_fee_percent: Decimal::from_str("0.0075").unwrap(),
+        staking_router_address: Addr::unchecked(VALID_ADDRESS_ONE),
+        page_limit: 1000,
+        paused: false,
+    };
+
+    let result = instantiate(deps.as_mut(), env, info, instantiate_message).unwrap_err();
+
+    assert_eq!(
+        result.to_string(),
+        "Error: fee collector allocations must add up to 1"
+    )
+}
+
+#[test]
 fn create_pair_with_valid_address_should_succeed() {
     let mut deps = mock_dependencies();
     let env = mock_env();
