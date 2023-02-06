e5e523d158e53db1ab86ed5ab4654b1f29deb5b2
James Gaddum <james@calculated.fi>
2023-02-06 14:02:37 +1300

    add initial fund contract

---
+++ contracts/fund/src/contract.rs
@@ -0,0 +1,36 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+// const CONTRACT_NAME: &str = "crates.io:fund";
+// const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub type ContractResult<T> = core::result::Result<T, ContractError>;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> ContractResult<Response> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> ContractResult<Response> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
