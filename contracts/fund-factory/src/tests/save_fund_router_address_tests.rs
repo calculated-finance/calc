3b32007beca3cc48874d709ea255a5e5a89024c1
aidan <aidan@calculated.fi>
2023-02-09 17:35:15 +1100

    multiple fund routers test

--- contracts/fund-factory/src/tests/save_fund_router_address_tests.rs
+++ contracts/fund-factory/src/tests/save_fund_router_address_tests.rs
@@ -13,7 +13,7 @@ use crate::{
 };
 
 #[test]
-fn saves_fund_router_address_indexed_by_owner_address() {
+fn saves_fund_router_address() {
     let mut mock_deps = mock_dependencies();
     let mock_env = mock_env();
     let mock_info = mock_info(USER, &vec![]);
@@ -55,4 +55,62 @@ fn saves_fund_router_address_indexed_by_owner_address() {
     let res: ManagedFundsResponse = from_binary(&binary).unwrap();
 
     assert_eq!(res.managed_funds[0], Addr::unchecked("test"));
+}
+
+#[test]
+fn saves_multiple_fund_router_addresses() {
+    let mut mock_deps = mock_dependencies();
+    let mock_env = mock_env();
+    let mock_info = mock_info(USER, &vec![]);
+
+    instantiate_contract(mock_deps.as_mut(), mock_env.clone(), mock_info.clone());
+
+    CACHE
+        .save(
+            mock_deps.as_mut().storage,
+            &Cache {
+                owner: Addr::unchecked(USER),
+            },
+        )
+        .unwrap();
+
+    save_fund_router_address(
+        mock_deps.as_mut(),
+        Reply {
+            id: AFTER_INSTANTIATE_FUND_ROUTER_REPLY_ID,
+            result: SubMsgResult::Ok(SubMsgResponse {
+                data: None,
+                events: vec![Event::new("instantiate").add_attribute("_contract_address", "test")],
+            }),
+        },
+    )
+    .unwrap();
+
+    save_fund_router_address(
+        mock_deps.as_mut(),
+        Reply {
+            id: AFTER_INSTANTIATE_FUND_ROUTER_REPLY_ID,
+            result: SubMsgResult::Ok(SubMsgResponse {
+                data: None,
+                events: vec![Event::new("instantiate").add_attribute("_contract_address", "test2")],
+            }),
+        },
+    )
+    .unwrap();
+
+    let get_managed_funds_by_address_msg = QueryMsg::GetManagedFundsByAddress {
+        address: Addr::unchecked(USER),
+    };
+
+    let binary = query(
+        mock_deps.as_ref(),
+        mock_env,
+        get_managed_funds_by_address_msg,
+    )
+    .unwrap();
+
+    let res: ManagedFundsResponse = from_binary(&binary).unwrap();
+
+    assert_eq!(res.managed_funds[0], Addr::unchecked("test"));
+    assert_eq!(res.managed_funds[1], Addr::unchecked("test2"));
 }
