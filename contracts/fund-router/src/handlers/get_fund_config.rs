ad936b5980ffbecd188b2b25c4da3145b5c6ec0e
aidan <aidan@calculated.fi>
2023-02-16 11:44:38 +1100

    refactor get fund config

--- contracts/fund-router/src/handlers/get_fund_config.rs
+++ contracts/fund-router/src/handlers/get_fund_config.rs
@@ -1,14 +1,14 @@
 use cosmwasm_std::Deps;
 use cosmwasm_std::StdResult;
 
-use super::get_fund::get_fund;
+use crate::state::funds::get_current_fund;
+
 use fund_core::msg::ConfigResponse as FundConfigResponse;
 use fund_core::msg::QueryMsg as FundQueryMsg;
 
 pub fn get_fund_config(deps: Deps) -> StdResult<FundConfigResponse> {
-    let fund = get_fund(deps)?
-        .address
-        .expect("config should be set after fund is assigned");
+    let fund =
+        get_current_fund(deps.storage)?.expect("config should be set after fund is assigned");
 
     deps.querier
         .query_wasm_smart(fund.clone(), &FundQueryMsg::GetConfig {})
