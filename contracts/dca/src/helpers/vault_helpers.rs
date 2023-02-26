adb649219c2cbf85cb2d80765f7837d0b6981443
James Gaddum <james@calculated.fi>
2023-02-27 12:45:21 +1300

    remove future func

--- contracts/dca/src/helpers/vault_helpers.rs
+++ contracts/dca/src/helpers/vault_helpers.rs
@@ -1,9 +1,5 @@
-use crate::{
-    handlers::get_events_by_resource_id::get_events_by_resource_id,
-    state::swap_adjustments::get_swap_adjustment, types::vault::Vault,
-};
-use base::events::event::EventData;
-use cosmwasm_std::{Coin, Decimal, Deps, StdResult, Uint128};
+use crate::{state::swap_adjustments::get_swap_adjustment, types::vault::Vault};
+use cosmwasm_std::{Coin, Deps, StdResult, Uint128};
 
 pub fn get_swap_amount(deps: &Deps, vault: Vault) -> StdResult<Coin> {
     let initial_amount = match vault.low_funds() {
@@ -29,30 +25,4 @@ pub fn get_swap_amount(deps: &Deps, vault: Vault) -> StdResult<Coin> {
 
 pub fn has_sufficient_funds(deps: &Deps, vault: Vault) -> StdResult<bool> {
     get_swap_amount(deps, vault).map(|swap_amount| swap_amount.amount > Uint128::new(50000))
-}
-
-pub fn get_standard_dca_received_amount(deps: Deps, vault: Vault) -> StdResult<Coin> {
-    let standard_dca_received_amount = get_events_by_resource_id(deps, vault.id, None, None)?
-        .events
-        .iter()
-        .fold(Uint128::zero(), |acc, event| match &event.data {
-            EventData::DcaPlusVaultExecutionCompleted {
-                sent,
-                received,
-                fee,
-                swap_adjustment,
-            } => {
-                let price = Decimal::from_ratio(sent.amount, received.amount);
-                let fee_percentage = Decimal::from_ratio(fee.amount, received.amount);
-                let unadjusted_swap_amount = sent.amount * (Decimal::one() / swap_adjustment);
-                let received_amount = unadjusted_swap_amount * (Decimal::one() / price);
-                acc + received_amount * (Decimal::one() - fee_percentage)
-            }
-            _ => acc,
-        });
-
-    Ok(Coin {
-        denom: vault.get_swap_denom(),
-        amount: standard_dca_received_amount,
-    })
 }
