70f90d4e6f2ebe143976f2c6c036008152d8f907
James Gaddum <james@calculated.fi>
2023-05-22 16:42:41 +0100

    add tests for trigger id by order id handler

--- contracts/dca/src/handlers/get_trigger_id_by_fin_limit_order_idx.rs
+++ contracts/dca/src/handlers/get_trigger_id_by_fin_limit_order_idx.rs
@@ -19,3 +19,64 @@ pub fn get_trigger_id_by_fin_limit_order_idx_handler(
         Err(StdError::generic_err("Trigger not found"))
     }
 }
+
+#[cfg(test)]
+mod get_trigger_id_by_fin_limit_order_idx_handler_tests {
+    use super::get_trigger_id_by_fin_limit_order_idx_handler;
+    use crate::{
+        state::triggers::save_trigger,
+        types::trigger::{Trigger, TriggerConfiguration},
+    };
+    use cosmwasm_std::{testing::mock_dependencies, Decimal, Uint128};
+
+    #[test]
+    fn returns_trigger_id() {
+        let mut deps = mock_dependencies();
+
+        let order_idx = Uint128::new(89);
+
+        save_trigger(
+            deps.as_mut().storage,
+            Trigger {
+                vault_id: Uint128::one(),
+                configuration: TriggerConfiguration::Price {
+                    target_price: Decimal::percent(200),
+                    order_idx: Some(order_idx),
+                },
+            },
+        )
+        .unwrap();
+
+        let response =
+            get_trigger_id_by_fin_limit_order_idx_handler(deps.as_ref(), order_idx).unwrap();
+
+        assert_eq!(response.trigger_id, Uint128::one());
+    }
+
+    #[test]
+    fn returns_error_if_trigger_not_found() {
+        let mut deps = mock_dependencies();
+
+        let order_idx = Uint128::new(89);
+
+        save_trigger(
+            deps.as_mut().storage,
+            Trigger {
+                vault_id: Uint128::one(),
+                configuration: TriggerConfiguration::Price {
+                    target_price: Decimal::percent(200),
+                    order_idx: Some(order_idx),
+                },
+            },
+        )
+        .unwrap();
+
+        let err = get_trigger_id_by_fin_limit_order_idx_handler(
+            deps.as_ref(),
+            order_idx + Uint128::one(),
+        )
+        .unwrap_err();
+
+        assert_eq!(err.to_string(), "Generic error: Trigger not found");
+    }
+}
