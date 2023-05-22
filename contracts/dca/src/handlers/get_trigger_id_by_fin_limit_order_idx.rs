bd6b87f51c168990588120eb7d9a5a0c3ba72446
James Gaddum <james@calculated.fi>
2023-05-22 16:37:05 +0100

    add fetch trigger buy limit order idx query

--- contracts/dca/src/handlers/get_trigger_id_by_fin_limit_order_idx.rs
+++ contracts/dca/src/handlers/get_trigger_id_by_fin_limit_order_idx.rs
@@ -1,15 +1,21 @@
-use crate::{msg::TriggerIdResponse, state::old_triggers::TRIGGER_ID_BY_FIN_LIMIT_ORDER_IDX};
+use crate::{msg::TriggerIdResponse, state::triggers::trigger_store};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::Uint128;
-use cosmwasm_std::{Deps, StdResult};
+use cosmwasm_std::{Deps, StdError, StdResult};
 
-pub fn get_trigger_id_by_fin_limit_order_idx(
+pub fn get_trigger_id_by_fin_limit_order_idx_handler(
     deps: Deps,
     order_idx: Uint128,
 ) -> StdResult<TriggerIdResponse> {
-    let trigger_id = TRIGGER_ID_BY_FIN_LIMIT_ORDER_IDX
-        .load(deps.storage, order_idx.into())?
-        .into();
+    let trigger_id = trigger_store()
+        .idx
+        .order_idx
+        .item(deps.storage, order_idx.into())?
+        .map(|(_, trigger)| trigger.vault_id);
 
-    Ok(TriggerIdResponse { trigger_id })
+    if let Some(trigger_id) = trigger_id {
+        Ok(TriggerIdResponse { trigger_id })
+    } else {
+        Err(StdError::generic_err("Trigger not found"))
+    }
 }
