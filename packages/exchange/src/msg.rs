6758ddc6eef27cdead47e411d8a9a086a6ab291c
James Gaddum <james@calculated.fi>
2023-05-31 11:42:51 +0100

    add create & get pairs to exchange API

--- packages/exchange/src/msg.rs
+++ packages/exchange/src/msg.rs
@@ -1,5 +1,7 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Coin, Decimal256, Uint128};
+use cosmwasm_std::{Binary, Coin, Decimal256, Uint128};
+
+use crate::pair::Pair;
 
 #[cw_serde]
 pub struct InstantiateMsg {}
@@ -9,11 +11,17 @@ pub enum ExecuteMsg {
     SubmitOrder { target_price: Decimal256 },
     RetractOrder { order_idx: Uint128 },
     WithdrawOrder { order_idx: Uint128 },
+    InternalMsg(Binary),
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
+    #[returns(Vec<Pair>)]
+    GetPairs {
+        limit: u16,
+        start_after: Option<Pair>,
+    },
     #[returns(OrderStatus)]
     GetOrderStatus { order_idx: Uint128 },
 }
